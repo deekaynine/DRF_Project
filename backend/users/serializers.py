78ba9de0eff379c008896baea64c6e66310c6ff6
Matthew Tse <matthewtse123work@gmail.com>
2024-03-22 23:16:48 -0400

    token serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,23 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from users.models import User, Profile
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    #pass class and user
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['full_name'] = user.full_name
+        token['email'] = user.email
+        token['username'] = user.username
+        try:
+            token['vendor_id'] = user.vendor_id
+        except:
+            token['vendor_id'] = 0
+
+        return token
+        
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:

7376eecc401b17341b20da13acccbc6c16cf5bb7
Matthew Tse <matthewtse123work@gmail.com>
2024-03-24 04:33:23 -0400

    semi done with password reset view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from users import views as user_views
 from store import views as store_views
 
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [   
     path('user/token/', user_views.MyTokenObtainPairView.as_view()),
-    path('user/register/', user_views.RegisterView.as_view())
+    path('user/token/refresh/', TokenRefreshView.as_view()),
+    path('user/register/', user_views.RegisterView.as_view()),
+    path('user/password-reset/<email>/', user_views.PasswordRestView.as_view())
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -4,7 +4,14 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from users.models import User, Profile
-from users.serializers import MyTokenObtainPairSerializer, RegisterSerializer
+from users.serializers import MyTokenObtainPairSerializer, RegisterSerializer, UserSerializer
+
+import shortuuid
+
+def generate_otp():
+    uuid_key = shortuuid.uuid()
+    unique_key = uuid_key[:6]
+    return unique_key
 
 # Create your views here.
 
@@ -15,3 +22,26 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = ([AllowAny])
     serializer_class = RegisterSerializer
+
+class PasswordRestView(generics.RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        email = self.kwargs['email']
+        user = User.objects.get(email=email)
+
+        print(user)
+
+        if user:
+            user.otp =  generate_otp()
+            user.save()
+
+        uidb64 = user.pk
+        otp = user.otp
+
+        link = f"http://localhost:5173/create-new-password?otp={otp}&uidb64={uidb64}"
+
+        # Send Email
+
+        return user

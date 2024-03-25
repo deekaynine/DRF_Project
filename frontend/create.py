6a175ca0be6274e496ce7f2f3a805c9e223f981a
Matthew Tse <matthewtse123work@gmail.com>
2024-03-25 03:27:34 -0400

    react setup

--- frontend/create.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-endpoint = "http://localhost:8000/api/products/"
-
-get_response = requests.post(endpoint, json={"title": "Hello world post"})
-print(get_response.json())
-
--- frontend/test.py
+++
@@ -1,4 +0,0 @@
-arr = [ 1, 2, 3, 4, 5]
-arr.pop(arr.index(max(arr)))
-arr.pop(arr.index(min(arr)))
-print(sum(arr))

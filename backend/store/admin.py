3168348f9e4836f711b7b338c236f2d1766b2489
Matthew Tse <matthewtse123work@gmail.com>
2024-04-21 05:11:44 -0400

    added inlines for products

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from store.models import Product, Category, Gallery, Specification, Size , Color
 
+class GalleryInline(admin.TabularInline):
+    model = Gallery
+
+class SpecificationInline(admin.TabularInline):
+    model = Specification
+
+class SizeInline(admin.TabularInline):
+    model = Size
+
+class ColorInline(admin.TabularInline):
+    model = Color
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'category', 'shipping_amount', 'in_stock', 'vendor', 'featured']
+    list_editable = ['featured']
+    list_filter = ['date']
+    search_fields = ['title']
+    inlines = [GalleryInline, SpecificationInline, SizeInline, ColorInline]
+
 # Register your models here.
 
-admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)

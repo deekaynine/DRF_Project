fda31cec4a8d4182583f64ef2a4402088cfad13d
Matthew Tse <matthewtse123work@gmail.com>
2024-04-24 04:04:11 -0400

    added serializers for store models

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from store.models import Product, Category
+from store.serializers import ProductFaqSerializer, CategorySerializer
+
+from rest_framework import generics
+
 # Create your views here.
+
+class CategoryListAPIView(generics.ListAPIView):
+    

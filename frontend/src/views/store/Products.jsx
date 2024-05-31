9944b2d2a9da45820133b3bbacf1a5be3aeef1c3
Matthew Tse <matthewtse123work@gmail.com>
2024-05-31 02:47:37 -0400

    pagination for products still need to make it reusable

--- frontend/src/views/store/Products.jsx
+++ frontend/src/views/store/Products.jsx
@@ -3,16 +3,15 @@ import { useSearchParams } from "react-router-dom"
 import { Link } from "react-router-dom"
 import apiInstance from "../../utils/axios"
 
-const PAGE_SIZE = 6
-
 function Products() {
+  const [searchParams, setSearchParams] = useSearchParams()
   const [products, setProducts] = useState([])
   const [category, setCategory] = useState([])
   const [productsCount, setProductsCount] = useState(0)
   const [pageCount, setPageCount] = useState(0)
-  const [currentPage, setCurrentPage] = useState(0)
-  const [searchParams, setSearchParams] = useSearchParams()
+  const [currentPage, setCurrentPage] = useState(searchParams.get("page") || 1)
   console.log(searchParams.get("page"))
+  console.log(currentPage)
 
   useEffect(() => {
     if (searchParams.get("page")) {
@@ -20,21 +19,23 @@ function Products() {
         .get(`products/?page=${searchParams.get("page")}`)
         .then((res) => {
           setProducts(res.data.results)
-          console.log(res.data.results)
+          setProductsCount(res.data.count)
+          setPageCount(Math.ceil(res.data.count / res.data.results?.length))
           setCurrentPage(searchParams.get("page"))
         })
     } else {
       apiInstance.get(`products/`).then((res) => {
         setProducts(res.data.results)
+        console.log(res.data)
         console.log(res.data.results)
         if (res.data.count !== 0) {
           setProductsCount(res.data.count)
-          setPageCount(Math.ceil(res.data.count / PAGE_SIZE))
+          setPageCount(Math.ceil(res.data.count / res.data.results?.length))
           setCurrentPage(1)
         }
       })
     }
-  }, [searchParams.get("page")])
+  }, [searchParams.get("page"), currentPage])
 
   useEffect(() => {
     apiInstance.get(`category/`).then((res) => {
@@ -112,10 +113,115 @@ function Products() {
               ))}
             </div>
             {/* Pagination */}
-            <div className="flex w-full border  ">
-              {pageCountHolder.map((page, index) => (
+
+            {pageCount >= 5 &&
+              currentPage > 3 &&
+              currentPage < pageCountHolder.length - 2 && (
+                <div className="flex w-full border  ">
+                  <>left</>
+                  <Link
+                    style={{
+                      textDecoration: "none",
+                      color: "black",
+                      marginInline: "3px",
+                      "font-size": "12px",
+                      content: "border-box",
+                    }}
+                    to={`http://localhost:5173/?page=${pageCountHolder[0]}`}
+                  >
+                    <button
+                      onClick={() => {
+                        console.log(currentPage)
+                      }}
+                      className="border px-2 py-2 "
+                    >
+                      {pageCountHolder[0]}
+                    </button>
+                  </Link>
+                  <span>...</span>
+                  {pageCountHolder
+                    .slice(
+                      currentPage - 3,
+                      pageCountHolder.length -
+                        (pageCountHolder.length - currentPage) +
+                        2
+                    )
+                    .map((page, index) => (
+                      <Link
+                        key={index}
+                        style={{
+                          textDecoration: "none",
+                          color: "black",
+                          marginInline: "3px",
+                          "font-size": "12px",
+                          content: "border-box",
+                        }}
+                        to={`http://localhost:5173/?page=${page}`}
+                      >
+                        <button
+                          onClick={() => console.log(currentPage)}
+                          className="border px-2 py-2 "
+                        >
+                          {page}
+                        </button>
+                      </Link>
+                    ))}
+                  <span>...</span>
+                  <Link
+                    style={{
+                      textDecoration: "none",
+                      color: "black",
+                      marginInline: "3px",
+                      "font-size": "12px",
+                      content: "border-box",
+                    }}
+                    to={`http://localhost:5173/?page=${pageCountHolder[pageCountHolder.length - 1]}`}
+                  >
+                    <button
+                      onClick={() => {
+                        console.log(currentPage)
+                      }}
+                      className="border px-2 py-2 "
+                    >
+                      {pageCountHolder[pageCountHolder.length - 1]}
+                    </button>
+                  </Link>
+                  <>right</>
+                </div>
+              )}
+
+            {pageCount >= 5 && currentPage == 3 && (
+              <div className="flex w-full border  ">
+                <>left</>
+                {pageCountHolder
+                  .slice(
+                    currentPage - 3,
+                    pageCountHolder.length -
+                      (pageCountHolder.length - currentPage) +
+                      2
+                  )
+                  .map((page, index) => (
+                    <Link
+                      key={index}
+                      style={{
+                        textDecoration: "none",
+                        color: "black",
+                        marginInline: "3px",
+                        "font-size": "12px",
+                        content: "border-box",
+                      }}
+                      to={`http://localhost:5173/?page=${page}`}
+                    >
+                      <button
+                        onClick={() => console.log(currentPage)}
+                        className="border px-2 py-2 "
+                      >
+                        {page}
+                      </button>
+                    </Link>
+                  ))}
+                <span>...</span>
                 <Link
-                  key={index}
                   style={{
                     textDecoration: "none",
                     color: "black",
@@ -123,17 +229,269 @@ function Products() {
                     "font-size": "12px",
                     content: "border-box",
                   }}
-                  to={`http://localhost:5173/?page=${page}`}
+                  to={`http://localhost:5173/?page=${pageCountHolder[pageCountHolder.length - 1]}`}
                 >
                   <button
-                    onClick={() => setSearchParams({ page: `${page}` })}
+                    onClick={() => {
+                      console.log(currentPage)
+                    }}
                     className="border px-2 py-2 "
                   >
-                    {page}
+                    {pageCountHolder[pageCountHolder.length - 1]}
                   </button>
                 </Link>
-              ))}
-            </div>
+                <>right</>
+              </div>
+            )}
+
+            {pageCount >= 5 && currentPage == 2 && (
+              <div className="flex w-full border  ">
+                <span>left</span>
+                {pageCountHolder.slice(0, 4).map((page, index) => (
+                  <Link
+                    key={index}
+                    style={{
+                      textDecoration: "none",
+                      color: "black",
+                      marginInline: "3px",
+                      "font-size": "12px",
+                      content: "border-box",
+                    }}
+                    to={`http://localhost:5173/?page=${page}`}
+                  >
+                    <button
+                      onClick={() => {
+                        console.log(currentPage)
+                      }}
+                      className="border px-2 py-2"
+                    >
+                      {page}
+                    </button>
+                  </Link>
+                ))}
+                <span>...</span>
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                    marginInline: "3px",
+                    "font-size": "12px",
+                    content: "border-box",
+                  }}
+                  to={`http://localhost:5173/?page=${pageCountHolder[pageCountHolder.length - 1]}`}
+                >
+                  <button
+                    onClick={() => {
+                      console.log(currentPage)
+                    }}
+                    className="border px-2 py-2 "
+                  >
+                    {pageCountHolder[pageCountHolder.length - 1]}
+                  </button>
+                </Link>
+                <span>right</span>
+              </div>
+            )}
+
+            {pageCount >= 5 && currentPage == 1 && (
+              <div className="flex w-full border  ">
+                {pageCountHolder.slice(0, 3).map((page, index) => (
+                  <Link
+                    key={index}
+                    style={{
+                      textDecoration: "none",
+                      color: "black",
+                      marginInline: "3px",
+                      "font-size": "12px",
+                      content: "border-box",
+                    }}
+                    to={`http://localhost:5173/?page=${page}`}
+                  >
+                    <button
+                      onClick={() => {
+                        console.log(currentPage)
+                      }}
+                      className="border px-2 py-2 "
+                    >
+                      {page}
+                    </button>
+                  </Link>
+                ))}
+                <span>...</span>
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                    marginInline: "3px",
+                    "font-size": "12px",
+                    content: "border-box",
+                  }}
+                  to={`http://localhost:5173/?page=${pageCountHolder[pageCountHolder.length - 1]}`}
+                >
+                  <button
+                    onClick={() => {
+                      console.log(currentPage)
+                    }}
+                    className="border px-2 py-2 "
+                  >
+                    {pageCountHolder[pageCountHolder.length - 1]}
+                  </button>
+                </Link>
+                <span>right</span>
+              </div>
+            )}
+            {pageCount >= 5 && currentPage == pageCountHolder.length && (
+              <div className="flex w-full border  ">
+                <span>left</span>
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                    marginInline: "3px",
+                    "font-size": "12px",
+                    content: "border-box",
+                  }}
+                  to={`http://localhost:5173/?page=${pageCountHolder[0]}`}
+                >
+                  <button
+                    onClick={() => {
+                      console.log(currentPage)
+                    }}
+                    className="border px-2 py-2 "
+                  >
+                    {pageCountHolder[0]}
+                  </button>
+                </Link>
+                <span>...</span>
+                {pageCountHolder.slice(-3).map((page, index) => (
+                  <Link
+                    key={index}
+                    style={{
+                      textDecoration: "none",
+                      color: "black",
+                      marginInline: "3px",
+                      "font-size": "12px",
+                      content: "border-box",
+                    }}
+                    to={`http://localhost:5173/?page=${page}`}
+                  >
+                    <button
+                      onClick={() => {
+                        console.log(currentPage)
+                      }}
+                      className="border px-2 py-2 "
+                    >
+                      {page}
+                    </button>
+                  </Link>
+                ))}
+              </div>
+            )}
+            {pageCount >= 5 && currentPage == pageCountHolder.length - 1 && (
+              <div className="flex w-full border  ">
+                <>left</>
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                    marginInline: "3px",
+                    "font-size": "12px",
+                    content: "border-box",
+                  }}
+                  to={`http://localhost:5173/?page=${pageCountHolder[0]}`}
+                >
+                  <button
+                    onClick={() => {
+                      console.log(currentPage)
+                    }}
+                    className="border px-2 py-2 "
+                  >
+                    {pageCountHolder[0]}
+                  </button>
+                </Link>
+                <span>...</span>
+                {pageCountHolder
+                  .slice(
+                    currentPage - 3,
+                    pageCountHolder.length -
+                      (pageCountHolder.length - currentPage) +
+                      2
+                  )
+                  .map((page, index) => (
+                    <Link
+                      key={index}
+                      style={{
+                        textDecoration: "none",
+                        color: "black",
+                        marginInline: "3px",
+                        "font-size": "12px",
+                        content: "border-box",
+                      }}
+                      to={`http://localhost:5173/?page=${page}`}
+                    >
+                      <button
+                        onClick={() => console.log(currentPage)}
+                        className="border px-2 py-2 "
+                      >
+                        {page}
+                      </button>
+                    </Link>
+                  ))}
+              </div>
+            )}
+            {pageCount >= 5 && currentPage == pageCountHolder.length - 2 && (
+              <div className="flex w-full border  ">
+                <>left</>
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                    marginInline: "3px",
+                    "font-size": "12px",
+                    content: "border-box",
+                  }}
+                  to={`http://localhost:5173/?page=${pageCountHolder[0]}`}
+                >
+                  <button
+                    onClick={() => {
+                      console.log(currentPage)
+                    }}
+                    className="border px-2 py-2 "
+                  >
+                    {pageCountHolder[0]}
+                  </button>
+                </Link>
+                <span>...</span>
+                {pageCountHolder
+                  .slice(
+                    currentPage - 3,
+                    pageCountHolder.length -
+                      (pageCountHolder.length - currentPage) +
+                      2
+                  )
+                  .map((page, index) => (
+                    <Link
+                      key={index}
+                      style={{
+                        textDecoration: "none",
+                        color: "black",
+                        marginInline: "3px",
+                        "font-size": "12px",
+                        content: "border-box",
+                      }}
+                      to={`http://localhost:5173/?page=${page}`}
+                    >
+                      <button
+                        onClick={() => console.log(currentPage)}
+                        className="border px-2 py-2 "
+                      >
+                        {page}
+                      </button>
+                    </Link>
+                  ))}
+              </div>
+            )}
+
             <br />
           </section>
           {/*Section: Wishlist*/}
